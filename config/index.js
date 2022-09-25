9b56d6537d88693dab4de935db7a2dcfdb171450
lixiangle <93477687+lixiangle@users.noreply.github.com>
2022-09-25 13:11:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGvRB5-YvKFljVOJf5mC4ZiBEYiY',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'a0qiEMabEZluSdE-eLqIkzVPhNSB37C8gKloJjTNieE',
       // 所在省份
       province: '甘肃',
       // 所在城市
